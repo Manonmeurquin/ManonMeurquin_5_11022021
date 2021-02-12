5bddcb355aa59c58b0a6e40261a495aceb78d3e5
Manon Meurquin <manon.meurquin@free.fr>
2021-02-12 14:12:12 +0100

    requete

--- js/index.js
+++ js/index.js
@@ -1,32 +1,48 @@
-const teddies = [
-    {
-        name:"Norbert",
-        price: 35,
-        imageUrl: "http://localhost:3000/images/teddy_1.jpg"
-    },
-    {
-        name:"eddy",
-        price: 35,
-        imageUrl: "http://localhost:3000/images/teddy_1.jpg"
-    },
-    {
-        name:"paul",
-        price: 35,
-        imageUrl: "http://localhost:3000/images/teddy_1.jpg"
-    },
-];
-const $teddiesList = document.querySelector(".teddies-list");
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://localhost:3000/api/teddies');
+xhr.send();
 
-teddies.forEach((teddy) => {
-    const $li = document.createElement("li");
-    const $name = document.createElement("p");
-    $name.innerText = `Nom : ${teddy.name}`;
-    const $price = document.createElement("p");
-    $price.innerText = `Prix : ${teddy.price}`;
-    const $img = document.createElement("img");
-    $img.src = teddy.imageUrl;
-    $li.appendChild($name);
-    $li.appendChild($price);
-    $li.appendChild($img);
-    $teddiesList.appendChild($li);
-})
+xhr.addEventListener('readystatechange', () => {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200 || xhr.status === 201){
+            console.log(xhr.responseText);
+            const teddies = JSON.parse(xhr.responseText);
+            console.log(teddies);
+            const $ul = document.querySelector('.teddies-list');
+
+            teddies.forEach((teddy) => {
+                const $li = document.createElement('li');
+                $ul.appendChild($li);
+
+                $name = document.createElement('p');
+                $name.innerText = `Nom : ${teddy.name}`;
+                $li.appendChild($name);
+
+                // Créer un tableau ? ou une liste ? rechercher option value, form...
+                // $colors = document.createElement([]);
+                // $colors.
+                // $li.appendChild($colors);
+
+                $description = document.createElement('p');
+                $description.innerText = `Description : ${teddy.description}`;
+                $li.appendChild($description);
+
+                $price = document.createElement('p');
+                $price.innerText = `Prix : ${teddy.price}`;
+                $li.appendChild($price);
+
+                $imageUrl = document.createElement('img');
+                $imageUrl.src = teddy.imageUrl;
+                $li.appendChild($imageUrl);
+         
+            });
+        } else {
+        }
+
+    }
+});
+
+xhr.addEventListener('error', (error) => {
+    console.log(error);
+    alert('Une erreur inconnue est survenue');
+});
